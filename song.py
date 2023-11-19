6346d2beb61a4422c7e96a886f454de26424cabc
yizhichengit <yizhi.chen1@my.jcu.edu.au>
2023-11-19 12:39:28 +0800

    A2: Creation of song class and testing of its functionality, including: 1. Test for creating empty songs by default, 2. Test for creating songs with given values, and 3. Mark songs as learned and not learned tests.

--- song.py
+++ song.py
@@ -5,5 +5,24 @@
 
 
 class Song:
-    """..."""
-    pass
+    def __init__(self, title="", artist="", year=0, is_learned=False):
+        """Constructor for the Song class."""
+        self.title = title
+        self.artist = artist
+        self.year = year
+        self.is_learned = is_learned
+
+    def __str__(self):
+        """String representation of the Song object."""
+        return f"{self.title} by {self.artist}, Released: {self.year}, Learned: {self.is_learned}"
+
+    def mark_as_learned(self):
+        """Mark the song as learned."""
+        self.is_learned = True
+
+    def mark_as_unlearned(self):
+        """Mark the song as unlearned."""
+        self.is_learned = False
+
+
+
--- test_song.py
+++ test_song.py
@@ -1,6 +1,5 @@
-"""(Incomplete) Tests for Song class."""
+# (Incomplete) Tests for Song class.
 from song import Song
-
 
 def run_tests():
     """Test Song class."""
@@ -15,10 +14,29 @@ def run_tests():
     assert not default_song.is_learned
 
     # Test initial-value song
+    print("\nTest initial-value song:")
     initial_song = Song("My Happiness", "Powderfinger", 1996, True)
-    # TODO: Write tests to show this initialisation works
+    print(initial_song)
+    assert initial_song.artist == "Powderfinger"
+    assert initial_song.title == "My Happiness"
+    assert initial_song.year == 1996
+    assert initial_song.is_learned
 
-    # TODO: Add more tests, as appropriate, for each method
+    # Test mark_as_learned and mark_as_unlearned methods
+    print("\nTest mark_as_learned and mark_as_unlearned methods:")
+    unlearned_song = Song("Unlearned Song", "Unknown Artist", 2000)
+    print(f"Before learning: {unlearned_song}")
+    assert not unlearned_song.is_learned
 
+    # Mark the song as learned
+    unlearned_song.mark_as_learned()
+    print(f"After learning: {unlearned_song}")
+    assert unlearned_song.is_learned
+
+    # Mark the song as unlearned
+    unlearned_song.mark_as_unlearned()
+    print(f"After unlearning: {unlearned_song}")
+    assert not unlearned_song.is_learned
 
 run_tests()
+
