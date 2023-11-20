d84e969a5c9986795dcec309ed3862b66d81ee3b
yizhichengit <yizhi.chen1@my.jcu.edu.au>
2023-11-20 18:49:16 +0800

    A2:Made some modifications and provided explanations for code comments

--- a1_classes.py
+++ a1_classes.py
@@ -4,7 +4,7 @@
 """
 Name:Yizhi Chen
 Date started:21/10/2023
-GitHub URL: https://github.com/yizhichengit/starter_a1_songs.git
+GitHub URL: https://github.com/yizhichengit/Assignment-2_Song-List-2.0/blob/master/a1_classes.py
 """
 
 
@@ -12,7 +12,7 @@ from song import Song
 from songcollection import SongCollection
 import json
 
-# 加载歌曲
+# Load Song
 def load_songs(filename):
     song_collection = SongCollection()
     with open(filename, 'r') as file:
@@ -22,21 +22,21 @@ def load_songs(filename):
             song_collection.add_song(song)
     return song_collection
 
-# 显示歌曲列表
+# Display song list
 def display_songs(song_collection):
     songs = song_collection.songs
     for i, song in enumerate(songs, 1):
         learned_status = 'learned' if song.is_learned else 'not learned'
         print(f"{i}. {song.title} by {song.artist} {song.year} ({learned_status})")
 
-# 添加新歌曲
+# add new songs
 def add_new_song(song_collection):
     title = input("Title: ")
     artist = input("Artist: ")
-    year = int(input("Year: "))  # 这里假设年份输入总是有效的整数
+    year = int(input("Year: "))  # Assuming that the year input is always a valid integer here
     song_collection.add_song(Song(title, artist, year))
 
-# 更改歌曲的学习状态
+# Change the learning status of the song
 def learn_song(song_collection):
     display_songs(song_collection)
     song_number = int(input("Enter the number of the song to mark as learned: "))
@@ -44,15 +44,15 @@ def learn_song(song_collection):
     song_to_learn.is_learned = True
     print(f"You have learned {song_to_learn.title}")
 
-# 保存歌曲到文件
+# Save Song to File
 def save_songs(song_collection, filename):
     songs_data = []
     for song in song_collection.songs:
-        songs_data.append(song.to_dict())  # 假设 Song 类有一个 to_dict 方法
+        songs_data.append(song.to_dict())  # Assuming the Song class has a to_ dict method
     with open(filename, 'w') as file:
         json.dump(songs_data, file, indent=4)
 
-# 主函数
+# main function
 def main():
     song_collection = load_songs("songs.json")
     while True:
@@ -70,7 +70,7 @@ def main():
         else:
             print("Invalid option")
 
-# 显示菜单
+# Display menu
 def print_menu():
     print("Menu:")
     print("D - Display songs")
