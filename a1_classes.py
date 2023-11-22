f72acf6eec664824cf1d58e285a871790f9c1a9c
yizhichengit <yizhi.chen1@my.jcu.edu.au>
2023-11-22 13:39:25 +0800

    A2:Review the main program and console files, add detailed comments and file descriptions to the main program and console, and provide explanations in some key code sections.

--- a1_classes.py
+++ a1_classes.py
@@ -1,19 +1,27 @@
-"""..."""
-# TODO: Copy your first assignment to this file, commit, then update to use Song class
-# Use SongCollection class if you want to
 """
-Name:Yizhi Chen
-Date started:21/10/2023
+Song List Manager.
+
+This script allows the user to manage a list of songs, including displaying,
+adding, and marking songs as learned. The songs are stored in a JSON file.
+and Very good use of song class and songcollection class.
+
+Name: Yizhi Chen
+Date started: 21/10/2023
 GitHub URL: https://github.com/yizhichengit/Assignment-2_Song-List-2.0/blob/master/a1_classes.py
 """
-
 
 from song import Song
 from songcollection import SongCollection
 import json
 
-# Load Song
+
 def load_songs(filename):
+    """
+    Load songs from a JSON file and return a SongCollection object.
+
+    :param filename: str - The name of the file to load songs from.
+    :return: SongCollection - The collection of songs.
+    """
     song_collection = SongCollection()
     with open(filename, 'r') as file:
         songs_data = json.load(file)
@@ -22,38 +30,60 @@ def load_songs(filename):
             song_collection.add_song(song)
     return song_collection
 
-# Display song list
+
 def display_songs(song_collection):
+    """
+    Display all songs in the collection with their number, title, artist, year, and learned status.
+
+    :param song_collection: SongCollection - The collection of songs to display.
+    """
     songs = song_collection.songs
     for i, song in enumerate(songs, 1):
         learned_status = 'learned' if song.is_learned else 'not learned'
         print(f"{i}. {song.title} by {song.artist} {song.year} ({learned_status})")
 
-# add new songs
+
 def add_new_song(song_collection):
+    """
+    Prompt the user to add a new song to the collection.
+
+    :param song_collection: SongCollection - The collection to add a new song to.
+    """
     title = input("Title: ")
     artist = input("Artist: ")
     year = int(input("Year: "))  # Assuming that the year input is always a valid integer here
     song_collection.add_song(Song(title, artist, year))
 
-# Change the learning status of the song
+
 def learn_song(song_collection):
+    """
+    Mark a song as learned based on user input.
+
+    :param song_collection: SongCollection - The collection of songs to update.
+    """
     display_songs(song_collection)
     song_number = int(input("Enter the number of the song to mark as learned: "))
     song_to_learn = song_collection.songs[song_number - 1]
     song_to_learn.is_learned = True
     print(f"You have learned {song_to_learn.title}")
 
-# Save Song to File
+
 def save_songs(song_collection, filename):
-    songs_data = []
-    for song in song_collection.songs:
-        songs_data.append(song.to_dict())  # Assuming the Song class has a to_ dict method
+    """
+    Save the songs in the collection to a JSON file.
+
+    :param song_collection: SongCollection - The collection of songs to save.
+    :param filename: str - The name of the file to save songs to.
+    """
+    songs_data = [song.to_dict() for song in song_collection.songs]  # Assumes the Song class has a to_dict method
     with open(filename, 'w') as file:
         json.dump(songs_data, file, indent=4)
 
-# main function
+
 def main():
+    """
+    Main function to run the song list manager.
+    """
     song_collection = load_songs("songs.json")
     while True:
         print_menu()
@@ -70,14 +100,17 @@ def main():
         else:
             print("Invalid option")
 
-# Display menu
+
 def print_menu():
+    """
+    Print the main menu options to the console.
+    """
     print("Menu:")
     print("D - Display songs")
     print("A - Add a song")
     print("L - Learn a song")
     print("Q - Quit")
 
+
 if __name__ == "__main__":
     main()
-
--- main.py
+++ main.py
@@ -15,59 +15,59 @@ GitHub URL:https://github.com/yizhichengit/Assignment-2_Song-List-2.0/blob/maste
 # main.py
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.spinner import Spinner
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
-from kivy.properties import StringProperty, ListProperty, ObjectProperty, NumericProperty
-from kivy.core.window import Window
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty
 from song import Song
 from songcollection import SongCollection
 import json
 
+# Define the main application class, which inherits from Kivy's App class.
 class SongListApp(App):
+    # Properties that will update the UI automatically when their values change.
     status_text = StringProperty('')
     song_collection = ObjectProperty(SongCollection())
     learned = NumericProperty(0)
     to_learn = NumericProperty(0)
 
+    # Build the application's UI from the KV file and load songs from the JSON file.
     def build(self):
         self.song_collection = SongCollection()
         self.song_collection.load_songs('songs.json')
-
         return Builder.load_file('app.kv')
 
-
+    # Load song data from a given filename and update the song list.
     def load_songs(self, filename):
         self.song_collection.load_songs(filename)
         self.update_song_list()
 
+    # Update the visible list of songs in the UI.
     def update_song_list(self):
-        # Update song list and status tags
+        # Clear the current list of song widgets.
         if self.root and hasattr(self.root.ids, 'songs_box'):
             self.root.ids.songs_box.clear_widgets()
+        # Create and add a button for each song to the song box.
         for song in self.song_collection.songs:
-            # Create a button and add it to the interface.
             button_color = (0, 1, 0, 1) if song.is_learned else (1, 0, 0, 1)
             button = Button(text=f'{song.title} by {song.artist} ({song.year})',
                             background_color=button_color,
                             on_release=self.toggle_learned)
-            button.song = song
+            button.song = song  # Attach the song object to the button for easy access.
             self.root.ids.songs_box.add_widget(button)
+        # Update the status labels for learned and to-learn counts.
         self.update_status_text()
 
+    # Add a new song to the collection with data from the input fields.
     def add_song(self):
         title = self.root.ids.title_input.text
         artist = self.root.ids.artist_input.text
         year_text = self.root.ids.year_input.text
 
-        # Error checking.
+        # Validate input fields are not empty.
         if not title or not artist or not year_text:
             self.status_text = "All fields must be completed"
             return
 
+        # Validate year input is a positive integer.
         try:
             year = int(year_text)
             if year <= 0:
@@ -77,51 +77,48 @@ class SongListApp(App):
             self.status_text = "Please enter a valid number"
             return
 
-        # add new songs
+        # Add the new song and clear the input fields.
         self.song_collection.add_song(Song(title, artist, year))
         self.update_song_list()
         self.clear_inputs()
 
+    # Clear the text inputs and reset the status text.
     def clear_inputs(self):
-        # Clear input fields
         self.root.ids.title_input.text = ""
         self.root.ids.artist_input.text = ""
         self.root.ids.year_input.text = ""
         self.status_text = ""
 
+    # Toggle the learned status of a song when its button is pressed.
     def toggle_learned(self, button_instance):
         song = button_instance.song
         song.is_learned = not song.is_learned
-        # Update the text of the status label.
-        self.status_text = "You have learned " if song.is_learned else "You need to learn " + song.title
-        self.update_song_list()
+        self.update_song_list()  # Refresh the list to reflect the change.
 
+    # Update the learned and to-learn counts based on the song collection.
     def update_status_text(self):
-        learned = len([song for song in self.song_collection.songs if song.is_learned])
-        to_learn = len(self.song_collection.songs) - learned
         self.learned = self.song_collection.get_number_of_learned_songs()
         self.to_learn = self.song_collection.get_number_of_unlearned_songs()
 
-
-
-
+    # Save the current list of songs to a JSON file when the app is closing.
     def on_stop(self):
-        # When the application is about to close, save the song to a JSON file.
         self.save_songs('songs.json')
 
+    # Write the song collection to a JSON file, converting each song to a dictionary.
     def save_songs(self, filename):
         with open(filename, 'w') as file:
             json.dump([song.to_dict() for song in self.song_collection.songs], file, indent=4)
 
+    # Sort the song list based on a given sort key and update the UI.
     def sort_songs(self, sort_key):
-
+        # Map the sort key from the UI to the actual song attribute name.
         sort_key_mapping = {'Artist': 'artist', 'Title': 'title', 'Year': 'year', 'Learned': 'is_learned'}
         actual_sort_key = sort_key_mapping.get(sort_key)
         if actual_sort_key:
             self.song_collection.sort(actual_sort_key)
-            self.update_song_list()
+            self.update_song_list()  # Refresh the song list in the UI.
 
+# Run the application.
 if __name__ == '__main__':
     SongListApp().run()
-
 
